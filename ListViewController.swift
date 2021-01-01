e3ef31fc7bdbb89783a6af9d9c4b8e84194a37e6
jsjphone8 <jsjphone8@gmail.com>
2021-01-01 23:00:57 +0900

    커스텀 프로토타입 뷰 - 영화 섬네일 추가 1) 프로토타입 셀을 custom 스타일로 지정 후 디자인 2) UITableViewCell을 상속받는 커스텀 클래스 정의 3) 프로토타입 셀과 커스텀 클래스 연결 4) 프로토타입 셀에 디자인된 객체를 연결한 커스텀 클래스의 아울렛 변수로 생성 5) 테이블 뷰 컨트롤러 클래스에서 커스텀 클래스를 이용하여 화면 데이터 처리

--- ListViewController.swift
+++ ListViewController.swift
@@ -2,21 +2,21 @@ import UIKit
 
 class ListViewController: UITableViewController {
     var dataset = [
-        ("다크나이트", "영웅물에 철학에 음악까지 더해져 예술이 되다.", "2008-09-04", 8.95),
-        ("호우시절", "때를 알고 내리는 좋은 비", "2009-10-08", 7.31),
-        ("말할 수 없는 비밀", "여기서 너까지 다섯 걸음", "2015-05-07", 9.19)
+        ("다크나이트", "영웅물에 철학에 음악까지 더해져 예술이 되다.", "2008-09-04", 8.95, "darknight.jpg"),
+        ("호우시절", "때를 알고 내리는 좋은 비", "2009-10-08", 7.31, "rain.jpg"),
+        ("말할 수 없는 비밀", "여기서 너까지 다섯 걸음", "2015-05-07", 9.19, "secret.jpg")
     ]
     
     // 테이블 뷰를 구성할 리스트 데이터
     lazy var list: [MovieVO] = {
         var datalist = [MovieVO]()
-        for (title, desc, opendate, rating) in dataset {
+        for (title, desc, opendate, rating, thumbnail) in dataset {
             let mvo = MovieVO()
             mvo.title = title
             mvo.description = desc
             mvo.opendate = opendate
             mvo.rating = rating
-            
+            mvo.thumbnail = thumbnail
             datalist.append(mvo)
         }
         return datalist
@@ -40,7 +40,7 @@ class ListViewController: UITableViewController {
         cell.desc?.text = row.description
         cell.opendate?.text = row.opendate
         cell.rating?.text = "\(row.rating!)"
-        
+        cell.thumbnail?.image = UIImage(named: row.thumbnail!)
         return cell
     }
     
--- MovieCell.swift
+++ MovieCell.swift
@@ -6,4 +6,5 @@ class MovieCell: UITableViewCell {
     @IBOutlet var desc: UILabel! // 영화에 대한 내용 설명
     @IBOutlet var opendate: UILabel! // 개봉일
     @IBOutlet var rating: UILabel! // 평점
+    @IBOutlet var thumbnail: UIImageView! // 섬네일 이미지
 }
