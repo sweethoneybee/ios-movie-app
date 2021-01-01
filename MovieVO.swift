c0fccfb95f1db92f3866ee0859bf9e636635c078
jsjphone8 <jsjphone8@gmail.com>
2021-01-01 21:47:56 +0900

    VO 객체 수정 후 추가 커밋이 꼬여서 영화 정보를 담은 MovieVO 클래스 파일 새로 생성

---
+++ MovieVO.swift
@@ -0,0 +1,17 @@
+//
+//  MovieVO.swift
+//  MyMovieChart
+//
+//  Created by 정성훈 on 2021/01/01.
+//
+
+import Foundation
+
+class MovieVO {
+    var thumbnail: String? // 영화 섬네일 이미지 주소
+    var title: String? // 영화 제목
+    var description: String? // 영화 제목
+    var detail: String? // 상세정보
+    var opendate: String? // 개봉일
+    var rating: Double? // 평점
+}
