e899f4ff230632801a42417edbdb9fce66ff66a0
jsjphone8 <jsjphone8@gmail.com>
2021-01-01 21:40:34 +0900

    영화 제목, 설명 테이블 뷰 구현 테이블 뷰 컨트롤러, 테이블 뷰, 내비게이션 뷰 컨트롤러로 구현 더미 데이터는 현재 하드코딩

---
+++ ListViewController.swift
@@ -0,0 +1,48 @@
+import UIKit
+
+class ListViewController: UITableViewController {
+    var dataset = [
+        ("다크나이트", "영웅물에 철학에 음악까지 더해져 예술이 되다.", "2008-09-04", 8.95),
+        ("호우시절", "때를 알고 내리는 좋은 비", "2009-10-08", 7.31),
+        ("말할 수 없는 비밀", "여기서 너까지 다섯 걸음", "2015-05-07", 9.19)
+    ]
+    
+    // 테이블 뷰를 구성할 리스트 데이터
+    lazy var list: [MovieVO] = {
+        var datalist = [MovieVO]()
+        for (title, desc, opendate, rating) in dataset {
+            let mvo = MovieVO()
+            mvo.title = title
+            mvo.description = desc
+            mvo.opendate = opendate
+            mvo.rating = rating
+            
+            datalist.append(mvo)
+        }
+        return datalist
+    }()
+    
+    override func viewDidLoad() {
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.list.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // 주어진 행에 맞는 데이터 소스를 읽어온다.
+        let row = self.list[indexPath.row]
+        // 테이블 셀 객체를 직접 생성하는 대신 큐로부터 가져옴
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ListCell")!
+        cell.textLabel?.text = row.title
+        
+        // 서브 타이틀에 데이터 연결
+        cell.detailTextLabel?.text = row.description
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        NSLog("선택된 행은 \(indexPath.row) 번째 행입니다.")
+    }
+}
---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMovieChart
+//
+//  Created by 정성훈 on 2021/01/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
