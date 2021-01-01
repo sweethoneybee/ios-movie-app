3caad2af27d87ebb7481d8e4b069f97567c2ab46
jsjphone8 <jsjphone8@gmail.com>
2021-01-01 22:41:42 +0900

    커스텀 프로토타입 셀 구현 UITableViewCell을 상속받은 커스텀 클래스로 구현

--- ListViewController.swift
+++ ListViewController.swift
@@ -33,21 +33,13 @@ class ListViewController: UITableViewController {
         // 주어진 행에 맞는 데이터 소스를 읽어온다.
         let row = self.list[indexPath.row]
         // 테이블 셀 객체를 직접 생성하는 대신 큐로부터 가져옴
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ListCell")!
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ListCell") as! MovieCell
         
-        // 영화 제목이 표시될 레이블을 title 변수로 받음
-        let title = cell.viewWithTag(101) as? UILabel
-        // 영화 요약이 표시될 레이블을 desc 변수로 받음
-        let desc = cell.viewWithTag(102) as? UILabel
-        // 영화 개봉일이 표시될 레이블을 opendate 변수로 받음
-        let opendate = cell.viewWithTag(103) as? UILabel
-        // 영화 별점이 표시될 레이블을 rating 변수로 받음
-        let rating = cell.viewWithTag(104) as? UILabel
-        // 데이터 소스에 저장된 값을 각 레이블 변수에 할당
-        title?.text = row.title
-        desc?.text = row.description
-        opendate?.text = row.opendate
-        rating?.text = "\(row.rating!)"
+        // 데이터 소스에 저장된 값을 각 아울렛 변수에 할당
+        cell.title?.text = row.title
+        cell.desc?.text = row.description
+        cell.opendate?.text = row.opendate
+        cell.rating?.text = "\(row.rating!)"
         
         return cell
     }
---
+++ MovieCell.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+class MovieCell: UITableViewCell {
+    
+    @IBOutlet var title: UILabel! // 영화 제목
+    @IBOutlet var desc: UILabel! // 영화에 대한 내용 설명
+    @IBOutlet var opendate: UILabel! // 개봉일
+    @IBOutlet var rating: UILabel! // 평점
+}
