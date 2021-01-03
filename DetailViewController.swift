84d44cef71d1618bf62591d8ab7c8c05d49f9899
jsjphone8 <jsjphone8@gmail.com>
2021-01-03 22:41:09 +0900

    영화 상세 페이지에 액티비티 인디케이터 추가 및 알림처리

--- DetailViewController.swift
+++ DetailViewController.swift
@@ -2,10 +2,14 @@ import UIKit
 import WebKit
 
 class DetailViewController: UIViewController {
+    @IBOutlet var spinner: UIActivityIndicatorView!
     @IBOutlet var wv: WKWebView!
     var mvo: MovieVO! // 목록 화면에서 전달하는 영화 정보를 받을 변수
     
     override func viewDidLoad() {
+        // WKNavigationDelegate의 델리게이트 객체를 지정
+        self.wv.navigationDelegate = self
+        
         NSLog("linkurl=\(self.mvo.detail!), title=\(self.mvo.title!)")
         
         // 내비게이션 바의 타이틀에 영화명을 출력한다.
@@ -37,6 +41,45 @@ class DetailViewController: UIViewController {
             }
             alert.addAction(cancelAction)
             self.present(alert, animated: true, completion: nil)
+        }
+    }
+}
+
+// MARK:- WKNavigationDelegate 프로토콜 구현
+extension DetailViewController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+        self.spinner.startAnimating() // 인디케이터 뷰의 애니메이션을 실행
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        self.spinner.stopAnimating() // 인디케이터 뷰의 애니메이션을 중단
+    }
+    
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        self.spinner.stopAnimating() // 인디케이터 뷰의 애니메이션을 중지
+        self.alert("상세 페이지를 읽어오지 못했습니다."){
+            _ = self.navigationController?.popViewController(animated: true)
+        }
+    }
+    
+    func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
+        self.spinner.stopAnimating()
+        self.alert("상세 페이지를 읽어오지 못했습니다."){
+            // 버튼 클릭 시, 이전 화면으로 되돌려 보낸다.
+            _ = self.navigationController?.popViewController(animated: true)
+        }
+    }
+}
+
+// MARK:- 심플한 경고창 함수 정의용 익스텐션
+extension UIViewController {
+    func alert(_ message: String, onClick: (() -> Void)? = nil) {
+        let controller = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        controller.addAction(UIAlertAction(title: "OK", style: .cancel){(_) in
+            onClick?()
+        })
+        DispatchQueue.main.async {
+            self.present(controller, animated: true)
         }
     }
 }
